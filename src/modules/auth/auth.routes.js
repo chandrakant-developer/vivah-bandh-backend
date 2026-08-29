2e2fac53dc923ec63392360730865586ea9d42e4
Chandrakant Patil <chandrakantpatil.in@gmail.com>
2026-08-29 23:08:31 +0530

    feat: refresh token api implementation

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -4,10 +4,12 @@ import { registerUserController } from './register/register.controller.js';
 import { validateRegisterUser } from './register/register.validator.js';
 
 import { loginController } from './login/login.controller.js';
+import { refreshController } from './refresh/refresh.controller.js';
 
 const router = express.Router();
 
 router.post('/register', validateRegisterUser, registerUserController);
 router.post('/login', loginController);
+router.post('/refresh', refreshController);
 
 export default router;
---
+++ src/modules/auth/refresh/refresh.service.js
@@ -0,0 +1,18 @@
+import { generateAccessToken, verifyRefreshToken } from '../../../utils/token.utils.js'
+import { REFRESH_ERRORS } from './refresh.errors.js';
+
+export const refreshService = (refreshToken) => {
+    try {
+        const payload = verifyRefreshToken(refreshToken);
+
+        const accessToken = generateAccessToken({
+            userId: payload.sub,
+        });
+
+        return {
+            accessToken,
+        }
+    } catch {
+        throw new Error(REFRESH_ERRORS.INVALID_REFRESH_TOKEN);
+    }
+};
